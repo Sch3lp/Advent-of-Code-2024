31a57df0a9d6dba05275813afc11d3db9d9da5e9
Sch3lp <sch3lp@gmail.com>
2024-11-30 20:19:15 +0100

    Added kotest property based testing dependency

--- buildSrc/src/main/kotlin/kotlin-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/kotlin-conventions.gradle.kts
@@ -1,3 +1,4 @@
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -21,9 +22,9 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 
     testImplementation("org.junit.jupiter:junit-jupiter:5.11.3")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.11.3")
-    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("io.kotest:kotest-property:$kotestVersion")
 }
 
 tasks.test {
@@ -31,8 +32,8 @@ tasks.test {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        languageVersion = "2.0"
-        freeCompilerArgs = listOf("-Xjsr305=strict")
+    compilerOptions {
+        languageVersion.set(KotlinVersion.KOTLIN_2_1)
+        freeCompilerArgs.add("-Xjsr305=strict")
     }
 }
