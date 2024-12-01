92f26d7c6ac5d64629cd028bb95d38fa0b60d130
Sch3lp <sch3lp@gmail.com>
2024-12-01 10:43:51 +0100

    Refactoring towards more explicitness

--- src/test/kotlin/be/swsb/aoc2024/day1/Day1Test.kt
+++ src/test/kotlin/be/swsb/aoc2024/day1/Day1Test.kt
@@ -1,7 +1,7 @@
 package be.swsb.aoc2024.day1
 
-import be.swsb.aoc2024.util.Debugging.debug
-import be.swsb.aoc2024.util.Debugging.withDebugging
+import be.swsb.aoc2024.day1.Distance.Companion.sum
+import be.swsb.aoc2024.day1.SimilarityScore.Companion.sum
 import be.swsb.aoc2024.util.readFile
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.equals.shouldBeEqual
@@ -30,18 +30,50 @@ class Day1Test : FunSpec({
     }
 })
 
+fun String.solve(): Long {
+    val (left, right) = extractLocationIdPairs()
+    val sortedLocationPairs = left.sorted().zip(right.sorted())
+    val distances = sortedLocationPairs.map(LocationPair::distance)
+    return distances.sum()
+}
+
 fun String.solve2(): Long {
-    val locationIdPairs = lines().map { line -> line.split("   ").let { it[0].toLong() to it[1].toLong() } }
-    val (left, right) = locationIdPairs.unzip()
-    val leftOccurrences = left.map { locationId -> locationId to right.count { locationId == it } }
-    val similarityScores = leftOccurrences.map { (it, occurrences) -> it * occurrences }
+    val (left, right) = extractLocationIdPairs()
+    val leftSimilarities = left.map { locationId -> locationId.similaritiesIn(right) }
+    val similarityScores = leftSimilarities.map(Similarity::similarityScore)
     return similarityScores.sum()
 }
 
-fun String.solve(): Long {
-    val locationIdPairs = lines().map { line -> line.split("   ").let { it[0].toLong() to it[1].toLong() } }
-    val (left, right) = locationIdPairs.unzip()
-    val sortedLocationIdPairs = left.sorted().zip(right.sorted())
-    val distances = sortedLocationIdPairs.map { (left,right) -> left.minus(right).absoluteValue }
-    return distances.sum()
+fun String.extractLocationIdPairs(): Pair<List<LocationId>, List<LocationId>> {
+    val locationIdPairs = lines().map { line -> line.split("   ").let { (left, right) -> left.toLocationId() to right.toLocationId() } }
+    return locationIdPairs.unzip()
+}
+
+@JvmInline
+value class LocationId(private val value: Long): Comparable<LocationId> {
+    override fun compareTo(other: LocationId): Int = this.value.compareTo(other.value)
+    fun distanceTo(other: LocationId) = Distance((this.value - other.value))
+    operator fun times(occurrences: Int): SimilarityScore = SimilarityScore(this.value * occurrences)
+    fun similaritiesIn(locationIds: List<LocationId>) = Similarity(this, locationIds.count { this == it })
+}
+private fun String.toLocationId() = LocationId(this.toLong())
+fun LocationPair.distance() = first.distanceTo(second)
+typealias LocationPair = Pair<LocationId, LocationId>
+
+data class Distance(private val value: Long): Comparable<Distance> {
+    private val absoluteValue = value.absoluteValue
+    override fun compareTo(other: Distance): Int = this.absoluteValue.compareTo(other.absoluteValue)
+    companion object {
+        fun List<Distance>.sum() = sumOf { it.absoluteValue }
+    }
+}
+
+data class Similarity(private val locationId: LocationId, private val occurrences: Int) {
+    val similarityScore: SimilarityScore get() = locationId * occurrences
+}
+
+@JvmInline value class SimilarityScore(private val value: Long) {
+    companion object {
+        fun List<SimilarityScore>.sum() = sumOf { it.value }
+    }
 }
