eff5e24344527156c0c5fd41a6ee797597f515c7
Sch3lp <sch3lp@gmail.com>
2024-12-01 10:18:39 +0100

    Solved day 1 part 2

--- src/test/kotlin/be/swsb/aoc2024/day1/Day1Test.kt
+++ src/test/kotlin/be/swsb/aoc2024/day1/Day1Test.kt
@@ -1,5 +1,7 @@
 package be.swsb.aoc2024.day1
 
+import be.swsb.aoc2024.util.Debugging.debug
+import be.swsb.aoc2024.util.Debugging.withDebugging
 import be.swsb.aoc2024.util.readFile
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.equals.shouldBeEqual
@@ -16,7 +18,25 @@ class Day1Test : FunSpec({
         val input = readFile("day1/input.txt")
         input.solve() shouldBeEqual 2756096
     }
+
+    test("example input part 2") {
+        val input = readFile("day1/exampleInput.txt")
+        input.solve2() shouldBeEqual 31
+    }
+
+    test("actual input part 2") {
+        val input = readFile("day1/input.txt")
+        input.solve2() shouldBeEqual 23117829
+    }
 })
+
+fun String.solve2(): Long {
+    val locationIdPairs = lines().map { line -> line.split("   ").let { it[0].toLong() to it[1].toLong() } }
+    val (left, right) = locationIdPairs.unzip()
+    val leftOccurrences = left.map { locationId -> locationId to right.count { locationId == it } }
+    val similarityScores = leftOccurrences.map { (it, occurrences) -> it * occurrences }
+    return similarityScores.sum()
+}
 
 fun String.solve(): Long {
     val locationIdPairs = lines().map { line -> line.split("   ").let { it[0].toLong() to it[1].toLong() } }
