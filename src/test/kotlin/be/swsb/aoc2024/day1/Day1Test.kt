a34fb90dfeb0cfd50c01a4c23d1b8dcf21ed5e4e
Sch3lp <sch3lp@gmail.com>
2024-12-01 10:05:30 +0100

    Solved day 1 part 1

--- src/test/kotlin/be/swsb/aoc2024/day1/Day1Test.kt
+++ src/test/kotlin/be/swsb/aoc2024/day1/Day1Test.kt
@@ -1,20 +1,27 @@
 package be.swsb.aoc2024.day1
 
 import be.swsb.aoc2024.util.readFile
+import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.equals.shouldBeEqual
-import org.junit.jupiter.api.Test
+import kotlin.math.absoluteValue
 
-class Day1Test {
+class Day1Test : FunSpec({
 
-    @Test
-    fun `example input part 1`() {
+    test("example input part 1") {
         val input = readFile("day1/exampleInput.txt")
-        input shouldBeEqual "snarf"
+        input.solve() shouldBeEqual 11
     }
 
-    @Test
-    fun `actual input part 1`() {
+    test("actual input part 1") {
         val input = readFile("day1/input.txt")
-        input shouldBeEqual "snarf snarf"
+        input.solve() shouldBeEqual 2756096
     }
+})
+
+fun String.solve(): Long {
+    val locationIdPairs = lines().map { line -> line.split("   ").let { it[0].toLong() to it[1].toLong() } }
+    val (left, right) = locationIdPairs.unzip()
+    val sortedLocationIdPairs = left.sorted().zip(right.sorted())
+    val distances = sortedLocationIdPairs.map { (left,right) -> left.minus(right).absoluteValue }
+    return distances.sum()
 }
