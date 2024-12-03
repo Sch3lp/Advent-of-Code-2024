5ea62f0d6584848c504df1a66893b13a1c64cc45
Sch3lp <sch3lp@gmail.com>
2024-12-03 11:03:46 +0100

    Tidying

--- src/test/kotlin/be/swsb/aoc2024/day3/Day3Test.kt
+++ src/test/kotlin/be/swsb/aoc2024/day3/Day3Test.kt
@@ -28,62 +28,56 @@ class Day3Test : FunSpec({
 
     test("actual input part 2") {
         val input = readFile("day3/input.txt")
-        input.solve2() shouldBeLessThan -1
+        input.solve2() shouldBeEqual 100189366
     }
 
     test("Instructions only executes correctly parsed multiplication") {
-        Instructions(listOf("mul(4,6)")).execute() shouldBe 24
-        Instructions(listOf("mul(11,8)")).execute() shouldBe 88
+        Instructions("mul(4,6)").execute() shouldBe 24
+        Instructions("mul(11,8)").execute() shouldBe 88
     }
 
     test("Instructions with conditionals only executes multiplication after do()") {
-        Instructions(listOf("mul(4,6)"), true).execute() shouldBe 24
-        Instructions(listOf("mul(11,8)"), true).execute() shouldBe 88
-        Instructions(listOf("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"), true).execute() shouldBe (2*4 + 8*5)
+        Instructions("mul(4,6)", true).execute() shouldBe 24
+        Instructions("mul(11,8)", true).execute() shouldBe 88
+        Instructions(
+            "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))",
+            true
+        ).execute() shouldBe (2 * 4 + 8 * 5)
     }
 
 })
 
 object Day3 {
     fun String.solve(): Long {
-        val instructions = Instructions(lines())
+        val instructions = Instructions(this)
         return instructions.execute()
     }
 
     fun String.solve2(): Long {
-        val instructions = Instructions(lines(), true)
+        val instructions = Instructions(this, true)
         return instructions.execute()
     }
 
-    class Instructions(private val lines: List<String>, private val useState: Boolean = false) {
+    class Instructions(private val input: String, private val useState: Boolean = false) {
         fun execute(): Long {
-            var state = State.Do
             val regex = if (useState) Regex("""mul\(\d+,\d+\)|do\(\)|don't\(\)""") else Regex("""mul\(\d+,\d+\)""")
-            return lines.asSequence().flatMap { line ->
-                regex
-                    .findAll(line)
-                    .map { it.value }
-                    .map {
-                        when (it) {
-                            "do()" -> {
-                                state = State.Do; 0
-                            }
-
-                            "don't()" -> {
-                                state = State.Dont; 0
-                            }
-
-                            else -> {
-                                if (state == State.Do) {
-                                    val (left, right) = it.drop("mul(".length).dropLast(1).split(",")
-                                    left.toLong() * right.toLong()
-                                } else 0
-                            }
+            return regex
+                .findAll(input)
+                .map { it.value }
+                .fold(State.Do to 0L) { (state, sum), it ->
+                    when {
+                        it == "do()" -> State.Do to sum
+                        it == "don't()" -> State.Dont to sum
+                        state == State.Do -> {
+                            val (left, right) = it.drop("mul(".length).dropLast(1).split(",")
+                            state to sum + (left.toLong() * right.toLong())
                         }
+                        else -> state to sum
                     }
-            }.sum()
+                }.second
         }
     }
+
 
     enum class State {
         Do, Dont
