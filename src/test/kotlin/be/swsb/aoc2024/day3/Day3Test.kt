6718431379ddd09bb534d4d18d15d0e745f6d5bd
Sch3lp <sch3lp@gmail.com>
2024-12-03 10:29:45 +0100

    Day 3 Part 2 - done (wasn't keeping conditional state across lines)

--- src/test/kotlin/be/swsb/aoc2024/day3/Day3Test.kt
+++ src/test/kotlin/be/swsb/aoc2024/day3/Day3Test.kt
@@ -28,52 +28,60 @@ class Day3Test : FunSpec({
 
     test("actual input part 2") {
         val input = readFile("day3/input.txt")
-        input.solve2() shouldBeLessThan 101681733
+        input.solve2() shouldBeLessThan -1
     }
 
     test("Instructions only executes correctly parsed multiplication") {
-        Instructions("mul(4,6)").execute() shouldBe 24
-        Instructions("mul(11,8)").execute() shouldBe 88
+        Instructions(listOf("mul(4,6)")).execute() shouldBe 24
+        Instructions(listOf("mul(11,8)")).execute() shouldBe 88
+    }
+
+    test("Instructions with conditionals only executes multiplication after do()") {
+        Instructions(listOf("mul(4,6)"), true).execute() shouldBe 24
+        Instructions(listOf("mul(11,8)"), true).execute() shouldBe 88
+        Instructions(listOf("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"), true).execute() shouldBe (2*4 + 8*5)
     }
 
 })
 
 object Day3 {
     fun String.solve(): Long {
-        val instructions = lines().map { line -> Instructions(line) }
-        return instructions.sumOf { it.execute() }
+        val instructions = Instructions(lines())
+        return instructions.execute()
     }
 
     fun String.solve2(): Long {
-        val instructions = lines().map { line -> Instructions(line, true) }
-        return instructions.sumOf { it.execute() }
+        val instructions = Instructions(lines(), true)
+        return instructions.execute()
     }
 
-    class Instructions(private val line: String, private val useState: Boolean = false) {
+    class Instructions(private val lines: List<String>, private val useState: Boolean = false) {
         fun execute(): Long {
             var state = State.Do
             val regex = if (useState) Regex("""mul\(\d+,\d+\)|do\(\)|don't\(\)""") else Regex("""mul\(\d+,\d+\)""")
-            return regex
-                .findAll(line)
-                .map { it.value }
-                .map {
-                    when (it) {
-                        "do()" -> {
-                            state = State.Do; 0
-                        }
+            return lines.asSequence().flatMap { line ->
+                regex
+                    .findAll(line)
+                    .map { it.value }
+                    .map {
+                        when (it) {
+                            "do()" -> {
+                                state = State.Do; 0
+                            }
 
-                        "don't()" -> {
-                            state = State.Dont; 0
-                        }
+                            "don't()" -> {
+                                state = State.Dont; 0
+                            }
 
-                        else -> {
-                            if (state == State.Do) {
-                                val (left, right) = it.drop("mul(".length).dropLast(1).split(",")
-                                left.toLong() * right.toLong()
-                            } else 0
+                            else -> {
+                                if (state == State.Do) {
+                                    val (left, right) = it.drop("mul(".length).dropLast(1).split(",")
+                                    left.toLong() * right.toLong()
+                                } else 0
+                            }
                         }
                     }
-                }.sum()
+            }.sum()
         }
     }
 
